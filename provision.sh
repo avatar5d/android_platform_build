7f00501ceb30c8da16cafd6f046161927c1505a1
avatar <avatar.android@gmail.com>
2010-07-05 15:28:38 -0700

    added scripts that hack around a couple build and deploy issues. ideally these wouldn't be necessary, but they get us up and running quickly.

---
+++ provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# prefer su from ChainsDD Superuser
+SU_MK=system/extras/su/Android.mk
+rm $SU_MK
+touch $SU_MK
+
+# tweak ChainsDD superuser Makefile. I'd rather use this
+# simple awk command than maintain my own branch
+SUPERUSER_APP_MK=packages/apps/Superuser/Android.mk
+sed "s/^LOCAL_MODULE_TAGS.*$//" $SUPERUSER_APP_MK > $SUPERUSER_APP_MK.new
+mv $SUPERUSER_APP_MK.new $SUPERUSER_APP_MK
+
+# updates the script that generates passion.mk to include
+# custom build.prop settings so the Market works properly.
+# this is just a hack because the devices/htc/passion.git
+# isn't mirrored on github, and i don't want to host my
+# own.
+cp build/setup-makefiles.sh device/htc/passion/setup-makefiles.sh
+
+# extract proprietary files
+pushd device/htc/passion/
+./unzip-files.sh
+popd
